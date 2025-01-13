8493a3ca3253e57fae18fbfb9974549efe96811f
Emma Giralt <emgico@prateducacio.com>
2025-01-13 20:13:05 +0100

    .

---
+++ AEA2/inicialitzacioparells.java
@@ -0,0 +1,20 @@
+package AEA2;
+
+public class inicialitzacioparells {
+    public static void main(String[] args) {
+        int[] parells = new int[100];
+        
+        for(int i = 0; i < parells.length; i++){
+            parells[i] = 2*i;
+            System.out.print(parells[i] + " ");
+        }
+
+
+        
+
+    }
+    
+}
+
+
+
