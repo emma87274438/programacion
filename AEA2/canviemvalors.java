15fed2aeb3329031a58896eb3ab3ca2f53ef6780
Emma Giralt <emgico@prateducacio.com>
2025-01-15 12:37:25 +0100

    .

---
+++ AEA2/canviemvalors.java
@@ -0,0 +1,30 @@
+package AEA2;
+
+public class canviemvalors {
+
+    public static void main(String[] args) {
+
+        int[][] matriu = new int [10][10];
+        for(int i=0; i< matriu.length; i++){
+            for(int j=0; j<matriu[i].length; j++){
+
+                if (i % 2 == 0 && j % 2 == 0) {
+                    matriu[i][j] = 1; 
+                } else if (i % 2 == 0 && j % 2 != 0) {
+                    matriu[i][j] = 2; 
+                } else if (i % 2 != 0 && j % 2 == 0) {
+                    matriu[i][j] = 3; 
+                } else {
+                    matriu[i][j] = 4; 
+                }
+
+                System.out.println("la posició " +i +" " +j +"de la matriu val:" + matriu[i][j]);
+            }
+
+           System.out.println();
+        }
+
+        
+
+    }
+}
