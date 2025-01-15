2d580fc882b456fce1927e815033b42312304ca6
Emma Giralt <emgico@prateducacio.com>
2025-01-15 12:24:05 +0100

    .

---
+++ AEA2/filescolumnes.java
@@ -0,0 +1,24 @@
+package AEA2;
+
+public class filescolumnes {
+
+    public static void main(String[] args) {
+
+        int[][] matriu = new int [10][10];
+        for(int i=0; i< matriu.length; i++){
+            for(int j=0; j<matriu[i].length; j++){
+
+                if ( i % 2==0 && j % 2==0){
+                    matriu[i][j]=-1;
+                }
+
+                System.out.println("la posició " +i +" " +j +"de la matriu val:" + matriu[i][j]);
+            }
+
+           System.out.println();
+        }
+
+        
+
+    }
+}
--- AEA2/trobarmaximin.java
+++ AEA2/trobarmaximin.java
@@ -4,16 +4,15 @@ package AEA2;
 public class trobarmaximin {
     public static void main(String[] args) {
     int[] array1 = {12,3,45,-3,65,-5,88,99,2,23,54,33,27,53,96,0};
-    
-    
+
     
     int lowestnum = array1[0];
 
-    int highestnum = array1[array1.length - 1];
+    int highestnum = array1[array1.length];
 
     System.out.println(array1.length);
 
-        for (int i = 0; i < array1.length - 1; i= i +1){
+        for (int i = 0; i < array1.length ; i++){
             if (lowestnum > array1[i]) {
             lowestnum = array1[i];
             }
